675fb7fd9514c296c6c4a82840ee1f08450fd893
J. Scott Berg <jsberg@bnl.gov>
2020-01-25 10:43:24 -0500

    Use lat_ele_locator to allow full element location syntax

--- locate.f90
+++ locate.f90
@@ -4,17 +4,17 @@ function ele_index(loc_str,lat) result(ix_ele)
   character(*), intent(in) :: loc_str
   type(lat_struct), target, intent(in) :: lat
   integer :: ix_ele
-  character(len(loc_str)) :: loc_up
-  integer :: i
+  integer :: n_loc
+  logical :: err
+  type(ele_pointer_struct), dimension(:), allocatable :: eles
 
-  loc_up = upcase(loc_str)
-  do i=0,lat%n_ele_max
-     if (loc_up.eq.lat%ele(i)%name) then
-        ix_ele = i
-        return
-     end if
-  end do
-  ix_ele = -1
+  call lat_ele_locator(loc_str,lat,eles,n_loc,err)
+  if (err.or.n_loc.ne.1) then
+     ix_ele = -1
+  else
+     ix_ele = eles(1)%loc%ix_ele
+  end if
+  if (allocated(eles)) deallocate(eles)
 end function ele_index
 
 function ele_pointer(loc_str,lat) result(ele_ptr)
@@ -23,20 +23,15 @@ function ele_pointer(loc_str,lat) result(ele_ptr)
   character(*), intent(in) :: loc_str
   type(lat_struct), target, intent(in) :: lat
   type(ele_struct), pointer :: ele_ptr
-  integer ix
-  interface
-     function ele_index(loc_str,lat) result(ix_ele)
-       use bmad
-       character(*), intent(in) :: loc_str
-       type(lat_struct), target, intent(in) :: lat
-       integer :: ix_ele
-     end function ele_index
-  end interface
-
-  ix = ele_index(loc_str,lat)
-  if (ix.eq.-1) then
+  integer :: n_loc
+  logical :: err
+  type(ele_pointer_struct), dimension(:), allocatable :: eles
+  
+  call lat_ele_locator(loc_str,lat,eles,n_loc,err)
+  if (err.or.n_loc.ne.1) then
      nullify(ele_ptr)
   else
-     ele_ptr => lat%ele(ix)
+     ele_ptr => eles(1)%ele
   end if
+  if (allocated(eles)) deallocate(eles)
 end function ele_pointer
