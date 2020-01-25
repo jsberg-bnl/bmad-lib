8dfc799c87ab0f1abb40d58c1f3d781a73fa4a80
J. Scott Berg <jsberg@bnl.gov>
2020-01-25 10:02:26 -0500

    Add function to call bmad_parser2 from argument

--- misc.f90
+++ misc.f90
@@ -13,3 +13,18 @@ subroutine bmad_parse_from_argument(i,lat)
     call bmad_parser(f,lat)
   end block
 end subroutine bmad_parse_from_argument
+subroutine bmad_parse2_from_argument(i,lat)
+  use bmad
+  implicit none
+  integer, intent(in) :: i
+  type(lat_struct), target, intent(inout) :: lat
+  integer :: n
+
+  if (command_argument_count().lt.i) stop 'Missing argument'
+  call get_command_argument(i,length=n)
+  block
+    character(n) :: f
+    call get_command_argument(i,value=f)
+    call bmad_parser2(f,lat)
+  end block
+end subroutine bmad_parse2_from_argument
